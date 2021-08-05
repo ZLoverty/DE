36dfb52b6898ea2592408c59e849ff89a3d910de
Zhengyang Liu <liuzy19911112@gmail.com>
2021-08-05 14:06:59 +0200

    Data analysis log code
    Summarize data analysis status in a table

---
+++ Code/utils.py
@@ -0,0 +1,47 @@
+from corrLib import readdata
+import pandas as pd
+
+def search_key_files(folder, key_ext='raw'):
+    """
+    Search for the key files, return the date and relative directory of the files, as DataFrame.
+    
+    Args:
+    folder -- main folder to search
+    key_ext -- file name extension of the key files to be searched 
+                default 'raw' is the raw image extension by bacteria.vi
+                
+    Returns:
+    dir_list -- (date, relative_dir) DataFrame
+    """
+    
+    date_list = []
+    sf_list = []
+    l = readdata(data_folder, 'raw')
+    for num, i in l.iterrows():
+        rdir = os.path.relpath(i.Dir, folder)
+        rdir_folder = os.path.split(rdir)[0]
+        date, sf = rdir_folder.split('/', 1)
+        date_list.append(date)
+        sf_list.append(sf)
+    
+    dir_list = pd.DataFrame({'Date': date_list, 'Subfolder': sf_list}).sort_values(by=['Date', 'Subfolder'])
+    
+    return dir_list
+
+
+def read_dropSize(dropSize_dir):
+    """
+    Read dropSize.xlsx file for inner and outer droplet sizes.
+    
+    Args:
+    dropSize_dir -- absolute directory of dropSize.xlsx
+    
+    Returns:
+    outer, inner -- outer and inner droplet sizes (um), rounded to 1 decimal place (numbers in row 5)
+    """
+    
+    s = pd.read_excel(io=dropSize_dir)
+    outer = np.around(s['outer'][5], 1)
+    inner = np.around(s['inner'][5], 1)
+    
+    return outer, inner
